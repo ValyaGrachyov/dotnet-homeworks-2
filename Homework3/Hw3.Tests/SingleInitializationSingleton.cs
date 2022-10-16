ad244e840ee04de843c19d398e27ec50a7fc3e3d
ValentineGrachyov <valentinegrachyov@gmail.com>
2022-10-17 00:02:06 +0300

    hw3

--- Homework3/Hw3.Tests/SingleInitializationSingleton.cs
+++ Homework3/Hw3.Tests/SingleInitializationSingleton.cs
@@ -1,35 +1,63 @@
 using System;
 using System.Threading;
 
-namespace Hw3.Tests;
-
-public class SingleInitializationSingleton
+namespace Hw3.Tests
 {
-    private static readonly object Locker = new();
 
-    private static volatile bool _isInitialized = false;
-
-    public const int DefaultDelay = 3_000;
-    
-    public int Delay { get; }
-
-    private SingleInitializationSingleton(int delay = DefaultDelay)
+    public class SingleInitializationSingleton
     {
-        Delay = delay;
-        // imitation of complex initialization logic
-        Thread.Sleep(delay);
+        private static readonly object Locker = new();
+
+        private static volatile bool _isInitialized = false;
+
+        public const int DefaultDelay = 3_000;
+        
+        private static Lazy<SingleInitializationSingleton> _instance =
+            new Lazy<SingleInitializationSingleton>(() => new SingleInitializationSingleton());
+
+        public int Delay { get; }
+
+        private SingleInitializationSingleton(int delay = DefaultDelay)
+        {
+            Delay = delay;
+            // imitation of complex initialization logic
+            Thread.Sleep(delay);
+        }
+
+        internal static void Reset()
+        {
+            new SingleInitializationSingleton();
+            _isInitialized = false;
+        }
+
+        public static void Initialize(int delay)
+        {
+            if (!_isInitialized)
+            {
+                lock (Locker)
+                {
+                    if (!_isInitialized)
+                    {
+                        lock (Locker)
+                        {
+                            _instance = new Lazy<SingleInitializationSingleton>(() =>
+                                new SingleInitializationSingleton(delay));
+                            _isInitialized = true;
+                        }
+                    }
+                    else 
+                    {
+                        throw new InvalidOperationException();
+                    }
+                }
+            }
+            else 
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public static SingleInitializationSingleton Instance => _instance.Value;
+
     }
-
-    internal static void Reset()
-    {
-        throw new NotImplementedException();
-    }
-
-    public static void Initialize(int delay)
-    {
-        throw new NotImplementedException();
-    }
-
-    public static SingleInitializationSingleton Instance => throw new NotImplementedException();
-
 }
--- Homework3/Hw3.Tests/StopWatcher.cs
+++ Homework3/Hw3.Tests/StopWatcher.cs
@@ -1,20 +1,22 @@
 using System;
 using System.Diagnostics;
 
-namespace Hw3.Tests;
-
-public static class StopWatcher
+namespace Hw3.Tests
 {
-    /// <summary>
-    /// For quick tests only. For benchmarks use
-    /// https://benchmarkdotnet.org/
-    /// </summary>
-    public static TimeSpan Stopwatch(Action action)
+
+    public static class StopWatcher
     {
-        var sw = new Stopwatch();
-        sw.Start();
-        action();
-        sw.Stop();
-        return sw.Elapsed;
+        /// <summary>
+        /// For quick tests only. For benchmarks use
+        /// https://benchmarkdotnet.org/
+        /// </summary>
+        public static TimeSpan Stopwatch(Action action)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            action();
+            sw.Stop();
+            return sw.Elapsed;
+        }
     }
 }
