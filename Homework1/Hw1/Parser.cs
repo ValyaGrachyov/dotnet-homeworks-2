b2f196d1e28b7cb9d5e32413dcada1746c338eed
ValentineGrachyov <valentinegrachyov@gmail.com>
2022-10-16 22:40:27 +0300

    hw2Conf

---
+++ Homework1/Hw1/Parser.cs
@@ -0,0 +1,34 @@
+using System;
+using Hw1;
+
+public static class Parser
+{
+    public static void ParseCalcArguments(string[] args, 
+        out double val1, 
+        out CalculatorOperation operation, 
+        out double val2)
+    { 
+        if(double.TryParse(args[0], out val1) == false) { throw new ArgumentException(); }        
+        
+        if(double.TryParse(args[2], out val2) == false) { throw new ArgumentException(); }
+        
+
+        if (args.Length > 3) { throw new ArgumentException(); }
+
+        switch (args[1])
+        {
+            case "+": operation = CalculatorOperation.Plus;
+                break;
+            case "-": operation = CalculatorOperation.Minus;
+                break;
+            case "*": operation = CalculatorOperation.Multiply;
+                break;
+            case "/": operation = CalculatorOperation.Divide;
+                break;
+            default: operation = CalculatorOperation.Undefined;
+                throw new InvalidOperationException();
+                break;
+        }
+    }
+
+}
