d49ba520857a25f0c74d82f4ca35e05301793155
ValentineGrachyov <valentinegrachyov@gmail.com>
2022-10-29 18:04:05 +0300

    hw3Test

---
+++ Homework3/Hw3.Mutex/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Diagnostics;
+using Hw3.Mutex;
+
+Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} {Process.GetCurrentProcess().Id} starts");
+using var wm = new WithMutex();
+Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} {Process.GetCurrentProcess().Id} acquires mutex");
+Console.WriteLine(wm.Increment());
+Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} {Process.GetCurrentProcess().Id} releases mutex");
---
+++ Homework3/Hw3.Tests/SingleInitializationSingleton.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Threading;
+
+namespace Hw3.Tests
+{
+
+    public class SingleInitializationSingleton
+    {
+        private static readonly object Locker = new();
+
+        private static volatile bool _isInitialized = false;
+
+        public const int DefaultDelay = 3_000;
+        
+        private static Lazy<SingleInitializationSingleton> _instance =
+            new Lazy<SingleInitializationSingleton>(() => new SingleInitializationSingleton());
+
+        public int Delay { get; }
+
+        private SingleInitializationSingleton(int delay = DefaultDelay)
+        {
+            Delay = delay;
+            // imitation of complex initialization logic
+            Thread.Sleep(delay);
+        }
+
+        internal static void Reset()
+        {
+            new SingleInitializationSingleton();
+            _isInitialized = false;
+        }
+
+        public static void Initialize(int delay)
+        {
+            if (!_isInitialized)
+            {
+                lock (Locker)
+                {
+                    if (!_isInitialized)
+                    {
+                        lock (Locker)
+                        {
+                            _instance = new Lazy<SingleInitializationSingleton>(() =>
+                                new SingleInitializationSingleton(delay));
+                            _isInitialized = true;
+                        }
+                    }
+                    else 
+                    {
+                        throw new InvalidOperationException();
+                    }
+                }
+            }
+            else 
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public static SingleInitializationSingleton Instance => _instance.Value;
+
+    }
+}
