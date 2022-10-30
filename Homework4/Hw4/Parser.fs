12e2682d4b2a66cc3c73bf2e15006d2c559456ae
ValentineGrachyov <valentinegrachyov@gmail.com>
2022-10-30 23:34:47 +0300

    Actions

---
+++ Homework4/Hw4/Parser.fs
@@ -0,0 +1,39 @@
+﻿module Hw4.Parser
+
+open System
+open System
+open Hw4.Calculator
+
+
+type CalcOptions = {
+    arg1: float
+    arg2: float
+    operation: CalculatorOperation
+}
+
+let isArgLengthSupported (args : string[]) =
+    args.Length = 3
+
+let parseOperation (arg : string) =
+   match arg with
+   | "+" -> CalculatorOperation.Plus
+   | "-" -> CalculatorOperation.Minus
+   | "*" -> CalculatorOperation.Multiply
+   | "/" -> CalculatorOperation.Divide
+   | _ ->  ArgumentException() |> raise
+  
+let parseToDouble (str:string) =
+    match Double.TryParse str with
+    | true,double -> Some double
+    | _ -> ArgumentException() |> raise
+
+let parseCalcArguments(args : string[]) =
+     if args = null || not(isArgLengthSupported args) then ArgumentException() |> raise
+     else
+     {
+        arg1 = (parseToDouble (args[0])).Value
+        operation = parseOperation (args[1])
+        arg2 = ( parseToDouble (args[2])).Value
+     }
+    
+    
---
+++ Homework4/Hw4/Program.fs
@@ -0,0 +1,14 @@
+﻿
+
+open System
+open Hw4.Calculator
+open Hw4.Parser
+
+let read = Console.ReadLine().Split(" ")
+
+let calculator CalcOptions =
+    calculate CalcOptions.arg1  CalcOptions.operation  CalcOptions.arg2
+
+
+Console.WriteLine(read |> parseCalcArguments |> calculator)
+
