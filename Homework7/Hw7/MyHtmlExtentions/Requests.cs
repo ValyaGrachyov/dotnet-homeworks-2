08807a259be712498bfdcb24d5132e22ad6afb39
ValentineGrachyov <valentinegrachyov@gmail.com>
2022-11-17 18:38:18 +0300

    hw7_Conflicts

---
+++ Homework7/Hw7/MyHtmlExtentions/Requests.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Hw7.MyHtmlExtentions
+{
+    public static class Requests
+    {
+        public static IHtmlContent GetReq(PropertyInfo[] properties)
+        {
+            var builder = new HtmlContentBuilder();
+
+            foreach (var property in properties)
+                builder.AppendHtmlLine($"<div>{HTMLBuilder.CreateForm(property)}<br></div>");
+
+            return builder;
+        }
+
+        public static IHtmlContent PostReq(PropertyInfo[] properties, object? model)
+        {
+            var builder = new HtmlContentBuilder();
+
+            foreach (var property in properties)
+            {
+                var validationAttributes = property.GetCustomAttributes<ValidationAttribute>();
+                var propertyValue = property.GetValue(model);
+
+                builder.AppendHtmlLine($"<div>{HTMLBuilder.CreateForm(property)}");
+
+                foreach (var validationAttribute in validationAttributes)
+                {
+                    if (!validationAttribute.IsValid(propertyValue))
+                    {
+                        builder.AppendHtmlLine(
+                            $"{HTMLBuilder.CreateLabel($"{property.Name}", string.Empty)}" +
+                            $"<span>{validationAttribute.ErrorMessage}</span>");
+                    }
+                }
+
+                builder.AppendHtmlLine("<br></div>");
+            }
+
+            return builder;
+        }
+
+
+        
+    }
+}
