35692039636bd9bfe02f30d376978be3c7eba1f4
ValentineGrachyov <valentinegrachyov@gmail.com>
2022-11-28 00:03:44 +0300

    actions

--- Homework9/Hw9/Infrastructure/Services/MathCalculator/IMathCalculatorService.cs
+++
@@ -1,8 +0,0 @@
-using Hw9.Infrastructure.Models;
-
-namespace Hw9.Services.MathCalculator;
-
-public interface IMathCalculatorService
-{
-    public Task<CalculationMathExpressionResultDto> CalculateMathExpressionAsync(string? expression);
-}
