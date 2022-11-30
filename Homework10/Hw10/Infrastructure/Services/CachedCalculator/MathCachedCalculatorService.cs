0fd1be579ade1af4f783aa40b60c06af16c3dafd
ValentineGrachyov <valentinegrachyov@gmail.com>
2022-11-30 22:40:04 +0300

    hw10_-Actions

---
+++ Homework10/Hw10/Infrastructure/Services/CachedCalculator/MathCachedCalculatorService.cs
@@ -0,0 +1,35 @@
+﻿using Hw10.DbModels;
+using Hw10.Dto;
+using Hw10.Services.MathCalculator;
+
+namespace Hw10.Services.CachedCalculator;
+
+public class MathCachedCalculatorService : IMathCalculatorService
+{
+	private readonly ApplicationContext _dbContext;
+	private readonly IMathCalculatorService _simpleCalculator;
+
+	public MathCachedCalculatorService(ApplicationContext dbContext, IMathCalculatorService simpleCalculator)
+	{
+		_dbContext = dbContext;
+		_simpleCalculator = simpleCalculator;
+	}
+
+	public async Task<CalculationMathExpressionResultDto> CalculateMathExpressionAsync(string? expression)
+	{
+		var solvingExpression = _dbContext.SolvingExpressions.Where(expr => expr.Expression == expression);
+
+		if(solvingExpression.Any())
+        {
+			await Task.Delay(1000);
+			return await Task.Run(() => new CalculationMathExpressionResultDto(solvingExpression.First().Result));
+        }
+
+		var result = await _simpleCalculator.CalculateMathExpressionAsync(expression);
+		if (!result.IsSuccess) return result;
+		_dbContext.SolvingExpressions.Add(new SolvingExpression(expression!, result.Result));
+		await _dbContext.SaveChangesAsync();
+
+		return result;
+	}
+}
