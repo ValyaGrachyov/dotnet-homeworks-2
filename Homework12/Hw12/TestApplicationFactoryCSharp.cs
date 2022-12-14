e7bb886518fc60ac11d84d526cdb04f5d0b8f64c
ValentineGrachyov <valentinegrachyov@gmail.com>
2022-12-14 12:51:03 +0300

    _

--- Homework12/Hw12/TestApplicationFactoryCSharp.cs
+++ Homework12/Hw12/TestApplicationFactoryCSharp.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Mvc.Testing;
+using Hw6;
+
 
 namespace Hw12;
 
-public class TestApplicationFactoryCSharp : WebApplicationFactory<TestApplicationFactoryCSharp>
+public class TestApplicationFactoryCSharp : WebApplicationFactory<Calculator>
     // TODO: replace generic argument with the right one
 {
 }
